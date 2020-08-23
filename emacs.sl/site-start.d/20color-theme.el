2be67ded3ab032a3db3cea51e42c709a1eeeaefc
Lin Sun <sunlin7@yahoo.com>
2020-08-24 06:23:39 +0800

    remove unused files which for valina emacs, unmaintained

--- emacs.sl/site-start.d/20color-theme.el
+++
@@ -1,4 +0,0 @@
-;; after the "custom-set-faces"
-;; (load-file "~/.emacs.sl/site-lisp/color-theme-blackboard.el")
-;; (require 'color-theme)
-;; (eval-after-load "color-theme" (color-theme-blackboard))
