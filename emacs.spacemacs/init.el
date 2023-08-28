ce26c587cd52217ef437ac17fff8cfe27c022253
Lin Sun <sunlin7@hotmail.com>
2023-08-28 05:00:03 +0000

    *emacs.spacemacs/init.el: remove local variables

--- emacs.spacemacs/init.el
+++ emacs.spacemacs/init.el
@@ -4,23 +4,22 @@
 
 (defvar sl-root-dir (file-name-directory load-file-name)
   "The root dir of the .emacs.sl .")
-(defvar sl-site-lisp-dir (expand-file-name "site-lisp" sl-root-dir)
-  "The site-lisp dir'.")
-(defvar sl-site-start-dir (expand-file-name  "site-start.d" sl-root-dir)
-  "This site-start.d dir.")
+
 (defvar sl-packages-list '()
   "The list of the package used for currrent configuration.")
+
 (defcustom sl-savefile-dir (expand-file-name "save.d" user-emacs-directory)
   "The directory to save user data."
   :group 'sl-emacs
   :type 'string)
 
-(add-to-list 'load-path sl-site-lisp-dir)
+(add-to-list 'load-path (expand-file-name "site-lisp" sl-root-dir))
 
 (mapc 'load (seq-uniq
              (mapcar
               (lambda (x) (substring x 0 (string-match "\\.elc?\\(\\.gz\\)?$" x)))
-              (directory-files sl-site-start-dir t "^[0-9].+\\.elc?\\(\\.gz\\)?$"))))
+              (directory-files (expand-file-name  "site-start.d" sl-root-dir)
+                               t "^[0-9].+\\.elc?\\(\\.gz\\)?$"))))
 
 (provide 'sl-init)
 ;;; init.el ends here
