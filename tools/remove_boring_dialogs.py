e593e7ce61ac8e8c156980b0ac57b4ce8093b7b6
Lin Sun <sunlin7@hotmail.com>
2024-03-25 06:59:52 +0000

    Add automation for Login Pages

--- tools/remove_boring_dialogs.py
+++ tools/remove_boring_dialogs.py
@@ -2,6 +2,9 @@
 Tools for removing the boring dialog.
 globals().update("MANUAL_START_THREAD=True") will suppress auto start the thread.
 '''
+import cv2
+import numpy as np
+import re
 import sys
 import time
 import ctypes
@@ -9,8 +12,10 @@ import threading
 import win32api
 import win32con
 import win32gui
+import copy
 from ctypes import windll, wintypes
 from PIL import ImageGrab as igrab
+from tesserocr import PyTessBaseAPI
 
 class ITimerExec(object):
     '''Interface for executable object'''
@@ -76,6 +81,82 @@ class TimerExecGlobalProDlg(ITimerExec):
         return False
 
 
+class TimerExecLoginPage(ITimerExec):
+    ''' Login Page automation. It rely on the follow packages:
+    tesserocr: https://github.com/simonflueckiger/tesserocr-windows_build/,
+      install by pipenv install <RELEASE_URL/package.whl>
+    tesserocr-data: https://github.com/tesseract-ocr/tessdata/blob/main/eng.traineddata
+      download and put on the work directoy.
+    '''
+    hwnd = None
+    def __init__(self, hwnd):
+        self.hwnd = hwnd
+
+    def run(self):
+        if (win32gui.GetForegroundWindow() != self.hwnd  # foreground switched
+            or not win32gui.IsWindowVisible(self.hwnd)):  # invisible
+            print("foreground changed or invisible window")
+            return True
+
+        if not re.match(".*Communications - Sign In.*",
+                        win32gui.GetWindowText(self.hwnd)):
+            print("browser tab switched")
+            return False        # browser tab switched
+
+        windll.user32.SetThreadDpiAwarenessContext(wintypes.HANDLE(-2))  # Toggle ON
+        wRect = win32gui.GetWindowRect(self.hwnd)
+        img = igrab.grab(wRect)
+        imgCv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        imgBlur = cv2.GaussianBlur(imgCv, (3, 3), 0)
+        imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+        imgBin = cv2.Canny(imgGray, 30, 100, apertureSize=3)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (6,3))
+        dilate = cv2.dilate(imgBin, kernel, iterations=4)
+        contours, _hierarchy = cv2.findContours(dilate, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        rectList = [cv2.boundingRect(x) for x in contours]
+        rectList.sort(key=lambda x: x[1]+x[3])  # sort by right-bottom
+        preY = 0
+        tess = PyTessBaseAPI()
+        for x in rectList:
+            Y = x[1]+x[3]
+            if Y <= preY:       # skip same line
+                continue
+    
+            preY = Y
+            ximg = img.crop((x[0], x[1], x[0]+x[2], x[1]+x[3]))
+            tess.SetImage(ximg)
+            txt = tess.GetUTF8Text()
+            # print(f'text:{txt}')
+            if txt in ["Username\n", "Password\n"]:
+                pos0 = (x[0], x[1]+int(x[3]*2))
+                pos = win32gui.ClientToScreen(self.hwnd, pos0)
+                print(f'try input on: {pos}')
+                cInfo = win32gui.GetCursorInfo()  # save the Cursor pos
+                win32api.SetCursor(None)          # hide the Cursor
+                win32api.SetCursorPos(pos)
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+                time.sleep(0.5)
+                win32api.keybd_event(win32con.VK_DOWN, 0, 0, 0)
+                win32api.keybd_event(win32con.VK_DOWN, 0, win32con.KEYEVENTF_KEYUP, 0)
+                time.sleep(0.1)
+                win32api.keybd_event(win32con.VK_RETURN, 0, 0, 0)
+                win32api.keybd_event(win32con.VK_RETURN, 0, win32con.KEYEVENTF_KEYUP, 0)
+                time.sleep(0.1)
+                win32api.keybd_event(win32con.VK_RETURN, 0, 0, 0)
+                win32api.keybd_event(win32con.VK_RETURN, 0, win32con.KEYEVENTF_KEYUP, 0)
+                win32api.SetCursorPos(cInfo[2])  # restore the Cursor
+                win32api.SetCursor(cInfo[1])
+                return False
+            elif any([txt == "Unable to sign in\n",
+                      re.match("We found some errors.*", txt)]):
+                print(f"incorrect login credits, stopping auto script")
+                return True
+    
+        print(f"Nothing to do, will try next round.")
+        return False
+
+
 def listen_foreground(cb=lambda *args:None):
     WinEventProcType = ctypes.WINFUNCTYPE(
         None,
@@ -138,13 +219,16 @@ if not globals().get('MANUAL_START_THREAD'):
             eObjs[hwnd].run()
         elif title == "GlobalProtect" and '#32770' == win32gui.GetClassName(hwnd):
             eObjs[hwnd] = TimerExecGlobalProDlg(hwnd)
+        elif re.match(".*Communications - Sign In.*", title):
+            eObjs[hwnd] = TimerExecLoginPage(hwnd)
 
     _listen_thread = threading.Thread(target=listen_foreground, args=(evtCB,), daemon=True)
     _listen_thread.start()
 
     def timerCB(eObjs):
         rkeys = []
-        for key,obj in eObjs.items():
+        objs = copy.copy(eObjs)
+        for key,obj in objs.items():
             if obj.run():
                 rkeys.append(key)
 
