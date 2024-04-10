dca1b9b4b465904db27a456b36d09a33686480b5
Lin Sun <sunlin7@hotmail.com>
2024-04-10 06:57:12 +0000

    win_hotkey_cycle_groups.py: refactor with hot key "Ctrl+'"

--- tools/win_hotkey_cycle_groups.py
+++ tools/win_hotkey_cycle_groups.py
@@ -1,4 +1,4 @@
-''' -*- encode:utf-8 -*-
+'''
 hotkey for windows to cycle taskbar groups.
 This tools depends on pywin32, pillow, numpy, python-opencv
 $ pythonw.exe win_hotkey_cycle_groups.py
@@ -10,6 +10,7 @@ import win32api
 import win32gui
 import win32con
 
+import logging
 import threading
 import win32process
 import numpy as np
@@ -43,24 +44,26 @@ class APPBARDATA(ctypes.Structure):
 
 def hotkey_main():
     '''The main function to for hotkey procedural.'''
-    win32gui.RegisterHotKey(None, HOTKEY_CYCLE_GRO_NEXT, MOD_NOREPEAT | win32con.MOD_WIN, VK_OEM_7)
-    win32gui.RegisterHotKey(None, HOTKEY_CYCLE_GRO_PREV, MOD_NOREPEAT | win32con.MOD_WIN | win32con.MOD_SHIFT, VK_OEM_7)
-    win32gui.RegisterHotKey(None, HOTKEY_CYCLE_GRO_LAST, MOD_NOREPEAT | win32con.MOD_WIN, VK_OEM_4)
+    win32gui.RegisterHotKey(None, HOTKEY_CYCLE_GRO_PREV, MOD_NOREPEAT | win32con.MOD_WIN, VK_OEM_7)
+    win32gui.RegisterHotKey(None, HOTKEY_CYCLE_GRO_NEXT, MOD_NOREPEAT | win32con.MOD_WIN | win32con.MOD_SHIFT, VK_OEM_7)
+    # win32gui.RegisterHotKey(None, HOTKEY_CYCLE_GRO_LAST, MOD_NOREPEAT | win32con.MOD_WIN, VK_OEM_4)
     windll.user32.SetThreadDpiAwarenessContext(wintypes.HANDLE(-2))  # Toggle ON
+    ts0 = time.time()           # last hotkey happened
     msg = ctypes.wintypes.MSG()
     while windll.user32.GetMessageW(ctypes.byref(msg), 0, 0, 0) != 0:
         if msg.message == win32con.WM_HOTKEY \
-           and msg.wParam in [HOTKEY_CYCLE_GRO_PREV, HOTKEY_CYCLE_GRO_NEXT, HOTKEY_CYCLE_GRO_LAST]:
+           and msg.wParam in [HOTKEY_CYCLE_GRO_PREV, HOTKEY_CYCLE_GRO_NEXT]:
             hwndDesktop = win32gui.GetDesktopWindow()
             hwndTaskbar = win32gui.FindWindow("Shell_TrayWnd", None)
             if not all([hwndDesktop, hwndTaskbar]):  # non-exists Windows Explore
-                print (f"missed desktop: {hwndDesktop}, or taskbar: {hwndTaskbar}", file=sys.stderr)
+                logging.error(f"missed desktop: {hwndDesktop}, or taskbar: {hwndTaskbar}")
                 continue
 
             rectDesktop = win32gui.GetWindowRect(hwndDesktop)
             rectTaskbar = win32gui.GetWindowRect(hwndTaskbar)
+            logging.debug(f"rectDesktop: {rectDesktop}, rectTaskbar: {rectTaskbar}")
             if rectTaskbar[3] > rectDesktop[3]:  # taskbar hided
-                print (f"taskbar hided", file=sys.stderr)
+                logging.error(f"taskbar hided")
                 continue
 
             H = int((rectTaskbar[3] - rectTaskbar[1]) / 6)
@@ -70,27 +73,35 @@ def hotkey_main():
             imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
             imgBin = cv2.Canny(imgGray, 30, 100, apertureSize=3)
             contours, _hierarchy = cv2.findContours(imgBin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            if (len(contours) < 2):  # less than two window groups on taskbar
-                print (f"less than 2 windows groups", file=sys.stderr)
+            if len(contours) < 2:  # less than two window groups on taskbar
+                logging.warning(f"less than 2 windows groups")
                 continue
 
             rectList = [cv2.boundingRect(x) for x in contours]
-            print(f"rectList: {rectList}")
+            logging.debug(f"rectList: {rectList}")
             rectList.sort()     # the contours maybe in reverse order
             activeIdx = next((idx for idx,val in enumerate(rectList) if val[2]>H), -1)
-            off = 1 if msg.wParam == HOTKEY_CYCLE_GRO_NEXT else -1 if msg.wParam == HOTKEY_CYCLE_GRO_PREV else -activeIdx-1
+            ts1 = time.time()
+            if ts1 - ts0 >= 1.0 and activeIdx != len(rectList) - 1:
+                off = -activeIdx-1  # non-continily operation, jump to tail
+            else:
+                off = 1 if msg.wParam == HOTKEY_CYCLE_GRO_NEXT else -1
+
             nextIdx = (activeIdx + off) % len(rectList)
-            print(f"activeIdx: {activeIdx}, off: {off}, nextIdx: {nextIdx}, hotkey: {msg.wParam}")
+            logging.debug(f"activeIdx: {activeIdx}, off: {off}, nextIdx: {nextIdx}, hotkey: {msg.wParam}")
             tagRect = rectList[nextIdx]
-            cInfo = win32gui.GetCursorInfo()  # save the Cursor pos
-            pos1 = win32gui.ClientToScreen(hwndTaskbar, (tagRect[0] + int(tagRect[2]/2), int(H*3)))
-            print (f"pos1: {pos1}")
-            if hwndTaskbar != win32gui.WindowFromPoint((0, 2398)):
-                print (f"taskbar covered by: {hwndTaskbar}", file=sys.stderr)
+            pos1 = win32gui.ClientToScreen(hwndTaskbar, (tagRect[0], H*3))
+            logging.debug(f"pos1: {pos1}")
+
+            hwnd = win32gui.WindowFromPoint(pos1)
+            if hwndTaskbar != hwnd and "MSTaskSwWClass" != win32gui.GetClassName(hwnd):
+                logging.error(f"taskbar covered by: {hwnd}")
                 continue
+
             ksSHIFT = win32api.GetAsyncKeyState(win32con.VK_SHIFT)
             if ksSHIFT & 0x8000:
                 win32api.keybd_event(win32con.VK_SHIFT, 0, win32con.KEYEVENTF_KEYUP, 0)
+            cInfo = win32gui.GetCursorInfo()  # save the Cursor pos
             win32api.SetCursor(None)
             win32api.SetCursorPos(pos1)
             win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0)
@@ -102,7 +113,7 @@ def hotkey_main():
             if ksSHIFT & 0x8000:
                 win32api.keybd_event(win32con.VK_SHIFT, 0, 0, 0)
             time.sleep(0.1)                 # wait for the animation
-
+            ts0 = ts1
         else:
             win32gui.TranslateMessage(ctypes.byref(msg))
             win32gui.DispatchMessage(ctypes.byref(msg))
@@ -125,6 +136,7 @@ if not globals().get('MANUAL_START_THREAD'):
     _thread.start()
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s %(filename)s:%(lineno)d %(message)s', level=logging.DEBUG)
     win32api.SetConsoleCtrlHandler(
         # return True to discontinue the signal
         lambda ct: [True, ct == win32con.CTRL_C_EVENT and stop()][0], True)
