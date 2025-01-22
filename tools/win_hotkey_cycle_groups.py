bcc2232d201ef8e9576d30410e3faef8fc3adbac
Lin Sun <sunlin7@hotmail.com>
2025-01-22 06:50:49 +0000

    Refactor the switch win group with a top_most win to grab the focus from elevated window

--- tools/win_hotkey_cycle_groups.py
+++ tools/win_hotkey_cycle_groups.py
@@ -45,95 +45,132 @@ class APPBARDATA(ctypes.Structure):
 
 def hotkey_main():
     '''The main function to for hotkey procedural.'''
-    win32gui.RegisterHotKey(None, HOTKEY_CYCLE_GRO_PREV, MOD_NOREPEAT | win32con.MOD_WIN, VK_OEM_7)
-    win32gui.RegisterHotKey(None, HOTKEY_CYCLE_GRO_NEXT, MOD_NOREPEAT | win32con.MOD_WIN | win32con.MOD_SHIFT, VK_OEM_7)
-    # win32gui.RegisterHotKey(None, HOTKEY_CYCLE_GRO_LAST, MOD_NOREPEAT | win32con.MOD_WIN, VK_OEM_4)
+    # DPI Awareness
     windll.user32.SetThreadDpiAwarenessContext(wintypes.HANDLE(-2))  # Toggle ON
-    ts0 = time.time()           # last hotkey happened
-    msg = ctypes.wintypes.MSG()
-    while windll.user32.GetMessageW(ctypes.byref(msg), 0, 0, 0) != 0:
-        if msg.message == win32con.WM_HOTKEY \
-           and msg.wParam in [HOTKEY_CYCLE_GRO_PREV, HOTKEY_CYCLE_GRO_NEXT]:
-            hwndDesktop = win32gui.GetDesktopWindow()
-            hwndTaskbar = win32gui.FindWindow("Shell_TrayWnd", None)
-            if not all([hwndDesktop, hwndTaskbar]):  # non-exists Windows Explore
-                logging.error(f"missed desktop: {hwndDesktop}, or taskbar: {hwndTaskbar}")
-                continue
 
-            rectDesktop = win32gui.GetWindowRect(hwndDesktop)
-            rectTaskbar = win32gui.GetWindowRect(hwndTaskbar)
-            logging.debug(f"rectDesktop: {rectDesktop}, rectTaskbar: {rectTaskbar}")
-            if rectTaskbar[3] > rectDesktop[3]:  # taskbar hided
-                logging.error(f"taskbar hided")
-                continue
+    # create and initialize window class
+    wndClass                = win32gui.WNDCLASS()
+    wndClass.lpfnWndProc    = wndProc
+    wndClass.hInstance      = win32api.GetModuleHandle()
+    wndClass.lpszClassName  = 'CycleWindowGroup'
 
-            H = int((rectTaskbar[3] - rectTaskbar[1]) / 6)
-            img = igrab.grab((rectTaskbar[0], rectTaskbar[3] - H, rectTaskbar[2], rectTaskbar[3]))
-            imgCv = np.array(img)
-            imgBlur = cv2.GaussianBlur(imgCv, (3, 3), 0)
-            imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-            imgBin = cv2.Canny(imgGray, 30, 100, apertureSize=3)
-            contours, _hierarchy = cv2.findContours(imgBin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            if len(contours) < 2:  # less than two window groups on taskbar
-                logging.warning(f"less than 2 windows groups")
-                continue
+    # register window class
+    wndClassAtom = None
+    try:
+        wndClassAtom = win32gui.RegisterClass(wndClass)
+    except Exception as e:
+        print(e)
+        raise e
 
-            rectList = [cv2.boundingRect(x) for x in contours]
-            logging.debug(f"H: {H}, rectList: {rectList}")
-            rectList.sort()     # the contours maybe in reverse order
-            # for a flashing group indicator, the Contours are near by, connect them to a large one
-            for idx in range (len(rectList) - 1, 0, -1):  # reverse
-                ra,rb = rectList[idx-1], rectList[idx]
-                if ra[0] + ra[2] >= rb[0]:  # near by
-                    ra = list(ra)           # conver tuple to list first
-                    ra[2] = rb[0]+rb[2]-ra[0]
-                    rectList[idx-1] = ra
-                    del rectList[idx]
+    hWindow = win32gui.CreateWindowEx(
+        win32con.WS_EX_TOPMOST | win32con.WS_EX_TOOLWINDOW,
+        wndClassAtom,  # NOTE: an atom indicates to use a wndProc, otherwise pywin32 will treat as msg map
+        'Python Win32 Window',
+        win32con.WS_POPUP,
+        0, 0, 1, 1, None, None, wndClass.hInstance, None)
 
-            activeIdx = next((idx for idx,val in enumerate(rectList) if val[2]>H and val[2]<2.5*H), -1)
-            ts1 = time.time()
-            if ts1 - ts0 >= 1.0 and activeIdx != len(rectList) - 1:
-                off = -activeIdx-1  # non-continily operation, jump to tail
-            else:
-                off = 1 if msg.wParam == HOTKEY_CYCLE_GRO_NEXT else -1
+    win32gui.RegisterHotKey(hWindow, HOTKEY_CYCLE_GRO_PREV, MOD_NOREPEAT | win32con.MOD_WIN, VK_OEM_7)
+    win32gui.RegisterHotKey(hWindow, HOTKEY_CYCLE_GRO_NEXT, MOD_NOREPEAT | win32con.MOD_WIN | win32con.MOD_SHIFT, VK_OEM_7)
+    # win32gui.RegisterHotKey(None, HOTKEY_CYCLE_GRO_LAST, MOD_NOREPEAT | win32con.MOD_WIN, VK_OEM_4)
 
-            nextIdx = (activeIdx + off) % len(rectList)
-            logging.debug(f"activeIdx: {activeIdx}, off: {off}, nextIdx: {nextIdx}, hotkey: {msg.wParam}")
-            tagRect = rectList[nextIdx]
-            pos1 = win32gui.ClientToScreen(hwndTaskbar, (tagRect[0], H*3))
-            logging.debug(f"pos1: {pos1}")
+    # Dispatch messages
+    win32gui.PumpMessages()
+    windll.user32.UnregisterHotKey(hWindow, HOTKEY_CYCLE_GRO_NEXT)
+    windll.user32.UnregisterHotKey(hWindow, HOTKEY_CYCLE_GRO_PREV)
 
-            hwnd = win32gui.WindowFromPoint(pos1)
-            if hwndTaskbar != win32gui.GetAncestor(hwnd, win32con.GA_ROOTOWNER):
-                logging.error(f"taskbar covered by: {hwnd}")
-                continue
 
-            ksSHIFT = win32api.GetAsyncKeyState(win32con.VK_SHIFT)
-            if ksSHIFT & 0x8000:
-                win32api.keybd_event(win32con.VK_SHIFT, 0, win32con.KEYEVENTF_KEYUP, 0)
-            cInfo = win32gui.GetCursorInfo()  # save the Cursor pos
-            win32api.SetCursor(None)
-            win32api.SetCursorPos(pos1)
-            win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0)
-            # NOTE: use the ABSOLUTE mouse event to avoid "Access is denied" exception
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN|win32con.MOUSEEVENTF_ABSOLUTE, pos1[0], pos1[1], 0, 0)
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP|win32con.MOUSEEVENTF_ABSOLUTE, pos1[0], pos1[1], 0, 0)
-            win32api.keybd_event(win32con.VK_CONTROL, 0, win32con.KEYEVENTF_KEYUP, 0)
-            win32api.SetCursorPos(cInfo[2])
-            win32api.SetCursor(cInfo[1])
-            if ksSHIFT & 0x8000:
-                win32api.keybd_event(win32con.VK_SHIFT, 0, 0, 0)
-            time.sleep(0.1)                 # wait for the animation
-            ts0 = ts1
+ts0 = time.time()           # last hotkey happened
+def wndProc(hWindow, message, wParam, lParam):
+    """Windows messasge procedual"""
+    if message == win32con.WM_DESTROY:
+        win32gui.PostQuitMessage(0)
+        return 0
+
+    elif message == win32con.WM_HOTKEY \
+           and wParam in [HOTKEY_CYCLE_GRO_PREV, HOTKEY_CYCLE_GRO_NEXT]:
+        hwndDesktop = win32gui.GetDesktopWindow()
+        hwndTaskbar = win32gui.FindWindow("Shell_TrayWnd", None)
+        if not all([hwndDesktop, hwndTaskbar]):  # non-exists Windows Explore
+            logging.error(f"missed desktop: {hwndDesktop}, or taskbar: {hwndTaskbar}")
+            return 0
+
+        rectDesktop = win32gui.GetWindowRect(hwndDesktop)
+        rectTaskbar = win32gui.GetWindowRect(hwndTaskbar)
+        logging.debug(f"rectDesktop: {rectDesktop}, rectTaskbar: {rectTaskbar}")
+        if rectTaskbar[3] > rectDesktop[3]:  # taskbar hided
+            logging.error(f"taskbar hided")
+            return 0
+
+        H = int((rectTaskbar[3] - rectTaskbar[1]) / 6)
+        img = igrab.grab((rectTaskbar[0], rectTaskbar[3] - H, rectTaskbar[2], rectTaskbar[3]))
+        imgCv = np.array(img)
+        imgBlur = cv2.GaussianBlur(imgCv, (3, 3), 0)
+        imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+        imgBin = cv2.Canny(imgGray, 30, 100, apertureSize=3)
+        contours, _hierarchy = cv2.findContours(imgBin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) < 2:  # less than two window groups on taskbar
+            logging.warning(f"less than 2 windows groups")
+            return 0
+
+        rectList = [cv2.boundingRect(x) for x in contours]
+        logging.debug(f"H: {H}, rectList: {rectList}")
+        rectList.sort()     # the contours maybe in reverse order
+        # for a flashing group indicator, the Contours are near by, connect them to a large one
+        for idx in range (len(rectList) - 1, 0, -1):  # reverse
+            ra,rb = rectList[idx-1], rectList[idx]
+            if ra[0] + ra[2] >= rb[0]:  # near by
+                ra = list(ra)           # conver tuple to list first
+                ra[2] = rb[0]+rb[2]-ra[0]
+                rectList[idx-1] = ra
+                del rectList[idx]
+
+        activeIdx = next((idx for idx,val in enumerate(rectList) if val[2]>H and val[2]<2.5*H), -1)
+        global ts0              # use the global one
+        ts1 = time.time()
+        if ts1 - ts0 >= 1.0 and activeIdx != len(rectList) - 1:
+            off = -activeIdx-1  # non-continily operation, jump to tail
         else:
-            win32gui.TranslateMessage(ctypes.byref(msg))
-            win32gui.DispatchMessage(ctypes.byref(msg))
+            off = 1 if wParam == HOTKEY_CYCLE_GRO_NEXT else -1
 
-    # unregister the keys
-    windll.user32.UnregisterHotKey(None, HOTKEY_CYCLE_GRO_NEXT)
-    windll.user32.UnregisterHotKey(None, HOTKEY_CYCLE_GRO_PREV)
+        nextIdx = (activeIdx + off) % len(rectList)
+        logging.debug(f"activeIdx: {activeIdx}, off: {off}, nextIdx: {nextIdx}, hotkey: {wParam}")
+        tagRect = rectList[nextIdx]
+        pos1 = win32gui.ClientToScreen(hwndTaskbar, (tagRect[0], H*3))
+        logging.debug(f"pos1: {pos1}")
 
-    return 0
+        hwnd = win32gui.WindowFromPoint(pos1)
+        if hwndTaskbar != win32gui.GetAncestor(hwnd, win32con.GA_ROOTOWNER):
+            logging.error(f"taskbar covered by: {hwnd}")
+            return 0
+
+        # Show the window on top to grab the forcus, otherwise it can NOT move the cursor for an elevated window
+        win32gui.ShowWindow(hWindow, win32con.SW_SHOW)
+        win32gui.SetForegroundWindow(hWindow)
+
+        ksSHIFT = win32api.GetAsyncKeyState(win32con.VK_SHIFT)
+        if ksSHIFT & 0x8000:
+            win32api.keybd_event(win32con.VK_SHIFT, 0, win32con.KEYEVENTF_KEYUP, 0)
+        cInfo = win32gui.GetCursorInfo()  # save the Cursor pos
+
+        win32api.SetCursor(None)
+        win32api.SetCursorPos(pos1)
+        win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        win32api.keybd_event(win32con.VK_CONTROL, 0, win32con.KEYEVENTF_KEYUP, 0)
+        win32api.SetCursorPos(cInfo[2])
+        win32api.SetCursor(cInfo[1])
+        if ksSHIFT & 0x8000:
+            win32api.keybd_event(win32con.VK_SHIFT, 0, 0, 0)
+
+        win32gui.ShowWindow(hWindow, win32con.SW_HIDE)
+        time.sleep(0.1)                 # wait for the animation
+        ts0 = ts1
+
+        return 0
+
+    return win32gui.DefWindowProc(hWindow, message, wParam, lParam)
+
 
 def stop():
     '''Stop the plugin'''
