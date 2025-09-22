9164dd0f4f31888b43be9a5547a6be22062c4c49
Lin Sun <sunlin7@hotmail.com>
2025-09-22 06:32:23 +0000

    Do NOT disable the xterm-mouse mode

--- emacs.spacemacs/dot-emacs-template.el
+++ emacs.spacemacs/dot-emacs-template.el
@@ -244,14 +244,24 @@
   "Update key binding in terminal for FRAME, `$showkey -a` for key sequence."
   (when (terminal-live-p (frame-terminal frame))
     (with-selected-frame frame
-      (define-key input-decode-map "[;5~" [C-backspace])
-      (define-key input-decode-map "[;6~" [C-S-backspace])
-      (define-key input-decode-map "[1;8u" [?\C-\M-%]))))
+      ;; (define-key input-decode-map "[;5~" [C-backspace])
+      ;; (define-key input-decode-map "[;6~" [C-S-backspace])
+      ;; (define-key input-decode-map "[1;5l"  [?\C-.])
+      ;; (define-key input-decode-map "[1;5n"  [?\C-,])
+      ;; (define-key input-decode-map "[96;5u"  [?\C-`])
+      ;; (define-key input-decode-map "[97;7u"  [?\C-\M-a])
+      ;; (define-key input-decode-map "[98;7u"  [?\C-\M-b])
+      ;; (define-key input-decode-map "[99;7u"  [?\C-\M-c])
+      ;; (define-key input-decode-map "[100;7u"  [?\C-\M-d])
+      ;; (define-key input-decode-map "[101;7u"  [?\C-\M-e])
+      ;; (define-key input-decode-map "[102;7u"  [?\C-\M-f])
+      ;; (define-key input-decode-map "[1;8u"  [?\C-\M-%])
+			)))
 
 (add-hook 'after-make-frame-functions #'sl-term-kdb-patch)
 (sl-term-kdb-patch (selected-frame)) ; patch 'after-make-frame-functions for the initialed term
 
-(add-to-list 'after-init-hook (apply-partially 'xterm-mouse-mode 0))
+;; (add-to-list 'after-init-hook (apply-partially 'xterm-mouse-mode 0))
 (add-to-list 'after-init-hook #'global-hungry-delete-mode)
 
 ;; dired-quick-sort integration with dired.
